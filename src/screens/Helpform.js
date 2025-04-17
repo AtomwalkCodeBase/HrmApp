87b99d6e17aeff8434c48ee2ecde50aafa0a1f62
SouvagyaDash29 <souvagyadash1999@gmail.com>
2025-04-17 19:04:02 +0530

    final push

--- src/screens/Helpform.js
+++ src/screens/Helpform.js
@@ -135,7 +135,7 @@ const Helpform = () => {
     formData.append("remarks", remark);
 
     if (fileUri) {
-      formData.append("submitted_file_1", {
+      formData.append("uploaded_file", {
         uri: fileUri,
         name: fileName || "supporting_document.jpg",
         type: fileMimeType || "image/jpeg",
